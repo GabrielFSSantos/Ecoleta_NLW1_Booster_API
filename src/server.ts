a436b40a22dd5ad01688fb3f8044ea4f810aa5de
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-11-09 21:06:43 -0300

    Create Routes and Controllers

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
 import express from 'express';
+import routes from './routes';
+import cors from  'cors';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de Usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  response.json([
-    'Gabriel',
-    'Felipe',
-    'Souza',
-    'Santos', 
-  ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333, () => {
   console.log('Server is running!!');
